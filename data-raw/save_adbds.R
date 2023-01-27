bfc4675606190a82b70be872dcff09aa05d7de7e
Preston Burns <prburns@Sarepta.com>
2023-01-27 16:24:12 -0500

    add test dataset into package for now

--- data-raw/save_adbds.R
+++ data-raw/save_adbds.R
@@ -1,4 +1,9 @@
 # test file from https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
-adbds <- read.csv("adbds.csv") 
- 
+adbds <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv') %>%
+  mutate(STRESN  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", STRESN*.0113, STRESN), #Convert μmol/L to mg/dL
+         STRESU  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", "mg/dL", STRESU),
+  ) %>%
+  mutate(BLFL = ifelse(VISIT == 'Screening', TRUE, FALSE)) %>% # add baseline column
+  ungroup()
+
 usethis::use_data(adbds, overwrite = TRUE)
