6c33e51794b8e642f1483644bac07d545a1d5eb5
Preston Burns <s.preston.burns@gmail.com>
2023-06-19 11:39:24 -0400

    specify utf-8 encoding on csv reads of test data to make cmd check happy

--- data-raw/save_adlb.R
+++ data-raw/save_adlb.R
@@ -1,9 +1,12 @@
-# test file from https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
-adlb <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv') %>%
-  mutate(STRESN  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", STRESN*.0113, STRESN), #Convert μmol/L to mg/dL
+# test file from
+# https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
+
+adlb <- read.csv("https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv",
+                 encoding = "UTF-8") %>%
+  mutate(STRESN  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", STRESN * .0113, STRESN), #Convert μmol/L to mg/dL
          STRESU  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", "mg/dL", STRESU),
   ) %>%
-  mutate(BLFL = ifelse(VISIT == 'Screening', TRUE, FALSE)) %>% # add baseline column
+  mutate(BLFL = ifelse(VISIT == "Screening", TRUE, FALSE)) %>% # add baseline column
   ungroup() %>%
   filter(!(TEST %in% c("Diastolic Blood Pressure",
                      "Heart Rate",
--- data-raw/save_adsl.R
+++ data-raw/save_adsl.R
@@ -1,9 +1,12 @@
-# test file from https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
-adsl <- read.csv('https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv') %>%
-  mutate(STRESN  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", STRESN*.0113, STRESN), #Convert μmol/L to mg/dL
+# test file from
+# https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv"
+
+adsl <- read.csv("https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/renderer-specific/adbds.csv",
+                 encoding = "UTF-8") %>%
+  mutate(STRESN  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", STRESN * .0113, STRESN), #Convert μmol/L to mg/dL
          STRESU  = ifelse(TEST == "Creatinine" & STRESU == "μmol/L", "mg/dL", STRESU),
   ) %>%
-  mutate(BLFL = ifelse(VISIT == 'Screening', TRUE, FALSE)) %>% # add baseline column
+  mutate(BLFL = ifelse(VISIT == "Screening", TRUE, FALSE)) %>% # add baseline column
   ungroup() %>%
   select(USUBJID:SAFFN) %>%
   distinct()
