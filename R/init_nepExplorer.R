a41955c10f4a1c6bbd1367a9f275b1551b7d8dbf
lgakava <Lovemore.Gakava@ucb.com>
2024-02-12 23:43:20 +0000

    First Draft of working init function included in standalone and integrated into ISG. - pending patient profile lab tests mapping - investigation into animation related obs

--- R/init_nepExplorer.R
+++ R/init_nepExplorer.R
@@ -11,40 +11,32 @@
 #' @export
 
 init_nepExplorer <- function(data, settings) {
-  
-  # Merge treatment with labs events.
-  print("What what")
-  print(data$dm)
-  print("checking vitals load")
-  print(head(data$vitals))
-  
-  print(settings[["vitals"]])
   vs_sub <-
-    data$vitals %>% 
+    data$vitals %>%
     filter(
-      settings[["vitals"]][['measure_col']] %in% 
+      settings[["vitals"]][['measure_col']] %in%
         c(
-          settings[["vitals"]]$measure_values$DIABP, 
+          settings[["vitals"]]$measure_values$DIABP,
           settings[["vitals"]]$measure_values$SYSBP
           )
       )
-  print("END OF VITALS FILTER")
+
   vs_labs <- data$labs %>% bind_rows(vs_sub)
-  
+
   dm_sub <- data$dm %>%
     dplyr::select(
       settings[["dm"]][["id_col"]],
       settings[["dm"]][["treatment_col"]]
     )
-  
+
   # left join to keep all rows in dm (even if there were no AEs)
   anly <- dm_sub %>%
     dplyr::left_join(
       vs_labs,
       by= c(settings[["labs"]][['id_col']],settings[["labs"]][["treatment_col"]])
     )
-  
+
   labs_settings <- settings
-  
+
   return(list(data = list(labs = anly), settings = labs_settings))
 }
