ba207f0a39ff56d1b3ea673deb914702a8d36731
lgakava <Lovemore.Gakava@ucb.com>
2024-02-12 22:51:33 +0000

    update inst files - yaml and isg integration and standalone

--- inst/examples/safetyGraphics_demo_app.R
+++ inst/examples/safetyGraphics_demo_app.R
@@ -1,9 +1,21 @@
-library(nepExplorer)
+library(dplyr)
+library(safetyGraphics)
 
-# Safety Graphics app with nep-explorer included
-charts <- c(
-  safetyGraphics::makeChartConfig(),
-  safetyGraphics::makeChartConfig(packages = "nepExplorer")
-)
-
-safetyGraphics::safetyGraphicsApp(charts = charts)
+  # Safety Graphics app with nep-explorer included
+  charts <- c(
+    safetyGraphics::makeChartConfig(),
+    safetyGraphics::makeChartConfig(packages = "nepExplorer")
+  )
+  
+  charts$nepexplorerMod$meta <- nepExplorer::meta_nepExplorer
+  
+  adam_adlbc <- safetyData::adam_adlbc |>
+    mutate(STRESU = gsub("[\\(\\)]", "", regmatches(PARAM, gregexpr("\\(.*?\\)", PARAM))[[1]]))
+  
+  safetyGraphics::safetyGraphicsApp(domainData = list(
+    labs = adam_adlbc,
+    aes = safetyData::adam_adae,
+    dm = safetyData::adam_adsl,
+    vitals = safetyData::adam_advs
+  ),
+  charts = charts)
--- inst/examples/standalone_app.R
+++ inst/examples/standalone_app.R
@@ -10,4 +10,20 @@ if (!interactive()) {
 }
 
 #launch nepexplorer  stand-alone  app
-create_nepexplorer_app() # see ?create_nepexplorer_app for more info
+
+neplab <- nepExplorer::adlb %>%
+  mutate(
+    PARAM = TEST,
+    BLFL = ifelse(BLFL==TRUE, "Y", BLFL)
+  )
+
+nepvitals <- nepExplorer::advs %>%
+  mutate(
+    PARAM = ifelse(TEST=="Systolic Blood Pressure", "Systolic Blood Pressure (mmHg)", ifelse(TEST=="Diastolic Blood Pressure", "Diastolic Blood Pressure (mmHg)", TEST)),
+    BLFL = ifelse(BLFL==TRUE, "Y", BLFL)
+  )
+
+unique(neplab$BLFL)
+
+create_nepexplorer_app(data = list(dm = nepExplorer::adsl, labs = neplab, vitals = nepvitals))
+# create_nepexplorer_app() # see ?create_nepexplorer_app for more info
