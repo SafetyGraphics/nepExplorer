e483c84305d5c917f71a3038bb6d417deac86669
lgakava <Lovemore.Gakava@ucb.com>
2024-02-12 23:19:06 +0000

    Update create_nepexplorer_app to include dm, labs and vitals.

--- R/create_nepexplorer_app.R
+++ R/create_nepexplorer_app.R
@@ -12,55 +12,57 @@
 #'
 #' @export
 create_nepexplorer_app <- function(
-    lab_df = nepExplorer::adbds,
+    data = list(dm = nepExplorer::adsl, labs = nepExplorer::adlb),
     settings = NULL,
-    runNow = TRUE
+    mapping = NULL,
+    runNow = TRUE,
+    ...
 ) {
-  # create default settings when settings is not defined by default
-  # I kept these consistent with safetycharts metadata, incase we wanto to switch to using default
-  # LAB and DM domains
-  if (is.null(settings)) {
-    settings <- list(
-      labs = list("id_col" = "USUBJID", "measure_col" = "TEST",
-                  "measure_values" = list("Creatinine" = "Creatinine",
-                                          "Cystatin C" = "Cystatin C",
-                                          "eGFR" = "eGFR",
-                                          "eGFRcys" = "eGFRcys",
-                                          "Albumin/Creatinine" = "Albumin/Creatinine",
-                                          "Bicarbonate" =  "Bicarbonate",
-                                          "Blood Urea Nitrogen" =  "Blood Urea Nitrogen",
-                                          "Calcium" = "Calcium",
-                                          "Chloride" = "Chloride",
-                                          "Phosphorus" = "Phosphorus",
-                                          "Potassium" = "Potassium",
-                                          "Sodium" =  "Sodium",
-                                          "Diastolic Blood Pressure" = "Diastolic Blood Pressure",
-                                          "Systolic Blood Pressure" = "Systolic Blood Pressure"),
-                  "value_col" = "STRESN", "unit_col" = "STRESU", "studyday_col" = "DY",
-                  "visit_col" = "VISIT", "visit_order_col" = "VISITN",
-                  "baseline_flag" = "BLFL", "normal_col_high" = "STNRHI",
-                  "id_col" = "USUBJID", "age_col" = "AGE", "sex_col" = "SEX",
-                  "race_col" = "RACE", "treatment_col" = "ARM")
+if (is.null(mapping)) {
+    mapping <- list(labs =list("id_col" = "USUBJID", "measure_col" = "PARAM",
+                               "measure_values" = list("CREAT" = "Creatinine",
+                                                       "Cystatin C" = "Cystatin C",
+                                                       "eGFR" = "eGFR",
+                                                       "eGFRcys" = "eGFRcys",
+                                                       "Albumin/Creatinine" = "Albumin/Creatinine",
+                                                       "Bicarbonate" =  "Bicarbonate",
+                                                       "Blood Urea Nitrogen" =  "Blood Urea Nitrogen",
+                                                       "Calcium" = "Calcium",
+                                                       "Chloride" = "Chloride",
+                                                       "Phosphorus" = "Phosphorus",
+                                                       "Potassium" = "Potassium",
+                                                       "Sodium" =  "Sodium",
+                                                       "Diastolic Blood Pressure" = "Diastolic Blood Pressure",
+                                                       "Systolic Blood Pressure" = "Systolic Blood Pressure"),
+                               "value_col" = "STRESN", "unit_col" = "STRESU", "studyday_col" = "DY",
+                               "visit_col" = "VISIT", "visit_order_col" = "VISITN",
+                               "baseline_flag" = "BLFL", "normal_col_high" = "STNRHI",
+                               "id_col" = "USUBJID", "age_col" = "AGE", "sex_col" = "SEX",
+                               "race_col" = "RACE", "treatment_col" = "ARM"),
+                    dm = list("id_col" = "USUBJID", "treatment_col" = "ARM" ),
+                    vitals = list("id_col" = "USUBJID", "treatment_col" = "ARM","measure_col" = "PARAM",
+                                  "measure_values" = list(  "DIABP" = "Diastolic Blood Pressure (mmHg)",
+                                                            "SYSBP" = "Systolic Blood Pressure (mmHg)") )
     )
   }
   ## create object containing data and setting to pass to server
   params <- reactive({
     list(
-      data = list(labs = lab_df),
-      settings = settings
+      data =data,
+      settings = mapping
     )
   })
-  
+
   app <- shinyApp(
     ui =  fluidPage(nepexplorer_ui("nep")),
     server = function(input, output, session) {
       callModule(nepexplorer_server, "nep", params)
     }
   )
-  
+
   if (runNow)
     runApp(app)
   else
     app
-  
+
 }
