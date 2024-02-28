a50e480cc06e26159c9bab0d3131cc60800edba1
Preston Burns <prburns@Sarepta.com>
2024-02-28 04:32:57 +0000

    remove trailing whitespace

--- inst/examples/safetyGraphics_demo_app.R
+++ inst/examples/safetyGraphics_demo_app.R
@@ -13,7 +13,7 @@ library(safetyGraphics)
   adam_adlbc <- safetyData::adam_adlbc |>
     mutate(STRESU = gsub("[\\(\\)]", "", regmatches(PARAM, gregexpr("\\(.*?\\)", PARAM))[[1]]),
            STRESU = ifelse(PARAM == "Creatinine (umol/L)", "mg/dL", STRESU), # convert creatine to mg/dL
-           AVAL = ifelse(PARAM == "Creatinine (umol/L)", AVAL * .0113, AVAL), 
+           AVAL = ifelse(PARAM == "Creatinine (umol/L)", AVAL * .0113, AVAL),
            PARAM = ifelse(PARAM == "Creatinine (umol/L)", "Creatinine (mg/dL)", PARAM))
   
   safetyGraphics::safetyGraphicsApp(domainData = list(
